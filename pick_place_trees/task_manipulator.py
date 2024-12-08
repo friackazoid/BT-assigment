1b564246429a60d004928c7dd7171bd3182f945a
Alina Stepina <astepina1787@gmail.com>
2024-12-08 22:52:42 +0100

    CHG: behavior_tree, task_manipulator; improve class names

--- pick_place_trees/task_manipulator.py
+++ pick_place_trees/task_manipulator.py
@@ -2,9 +2,9 @@ import py_trees
 
 from pick_place_trees.mock_manipulator import MockManipulator
 
-class CalculateManipulatorPosition(py_trees.behaviour.Behaviour):
+class ManipulatorCalculatePosition(py_trees.behaviour.Behaviour):
     def __init__(self, name="Calculate Pick Position", manipulator: MockManipulator = None, key_object_position: str = "", object_position: tuple[float, float, float] = None):
-        super(CalculateManipulatorPosition, self).__init__(name=name)
+        super(ManipulatorCalculatePosition, self).__init__(name=name)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
         self.key_manipulator_target_position = "manipulator_target"
@@ -31,30 +31,27 @@ class CalculateManipulatorPosition(py_trees.behaviour.Behaviour):
         position = self.manipulator.get_grasp_position_for(object_position)
         setattr(self.blackboard, self.key_manipulator_target_position, position)
 
-        self.logger.debug(f"Calculated manipulator target position: [{position}] for object position: [{object_position}]")
+        self.logger.info(f"Calculated manipulator target position: [{position}] for object position: [{object_position}]")
         return py_trees.common.Status.SUCCESS
 
 
-class MoveToPosition(py_trees.behaviour.Behaviour):
+class ManipulatorMoveToPosition(py_trees.behaviour.Behaviour):
     def __init__(self, name="Move to Position", manipulator: MockManipulator = None, key_target_pose: str = "", target_position: tuple[float, float, float] = None):
-        super(MoveToPosition, self).__init__(name=name)
-        
+        super(ManipulatorMoveToPosition, self).__init__(name=name)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
         
         self.manipulator = manipulator
         self.key_target_pose = key_target_pose
         self.target_position = target_position
-        self._position_reached = False
 
-        self.blackboard = self.attach_blackboard_client(name=self.__class__.__name__)
         if self.key_target_pose != "":
+            self.blackboard = self.attach_blackboard_client(name=self.__class__.__name__)
             self.blackboard.register_key(key=self.key_target_pose, access=py_trees.common.Access.READ)
 
     def update(self) -> py_trees.common.Status:
         """
         Moves the manipulator to a target position.
         """
-
         target_position = None
         if self.target_position is not None:
             target_position = self.target_position
@@ -62,11 +59,8 @@ class MoveToPosition(py_trees.behaviour.Behaviour):
             target_position = getattr(self.blackboard, self.key_target_pose, None)
 
         if self.manipulator.move_to_position(target_position=target_position):
-            self._position_reached = True
-            self.logger.debug(f"Manipulator moved to target position: [{target_position}]")
+            self.logger.info(f"Manipulator moved to target position: [{target_position}]")
             return py_trees.common.Status.SUCCESS
-        else:
-            self._position_reached = False
-            self.logger.debug("Manipulator failed to move to target position: [{target_position}]")
 
+        self.logger.info("Manipulator failed to move to target position: [{target_position}]")
         return py_trees.common.Status.FAILURE
